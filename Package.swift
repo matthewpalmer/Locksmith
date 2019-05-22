a7d3b3ab6babe6f2ef9d80b2e9781eae7bed6d9e
Ben Lachman <blachman@mac.com>
2019-05-22 17:55:10 -0400

    Adds source path

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
       // Targets can depend on other targets in this package, and on products in packages which this package depends on.
       .target(
           name: "Locksmith",
-          dependencies: [])
+          dependencies: [],
+          path: "Source")
   ]
 )
