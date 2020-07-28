340b5efe1fd06ed563066c6942e2a6390881aaef
Colin Masters <colin.p.masters@gmail.com>
2020-07-28 15:30:28 -0700

    Updating Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(name: "Locksmith", targets: ["Locksmith"])
+    ],
+    targets: [
+        .target(name: "MyLibrary", dependencies: ["Utility"]),
+        .testTarget(name: "MyLibraryTests", dependencies: ["MyLibrary"])
+    ]
 )
