c2477484052857c20758a460493cd8c5d2564c07
João Eduardo <joao_eduardo_ss@icloud.com>
2020-10-29 12:15:39 -0300

    update package.swift for new swift-tools

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+  name: "Locksmith",
+	products: [
+		.library(name: "Locksmith", targets: ["Locksmith"])
+	],
+	targets: [
+		.target(name: "Locksmith", path: "Source")
+	]
 )
