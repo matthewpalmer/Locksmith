486dcedbe664c55a877e1d6fb01bde9f3b86ba92
Semen Osipov <semen.osipov@arealidea.ru>
2022-11-16 12:58:34 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"]),
+    ],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Source"),
+        .testTarget(
+            name: "LocksmithTests",
+            dependencies: ["Locksmith"]),
+    ]
 )
