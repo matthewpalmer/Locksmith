2ad26f4703ef5e5423088d75eb4817d4faf0ee0f
Jonathan Favorite <jonathan.favorite@teamsoftware.com>
2024-03-22 16:04:06 -0400

    SPM compatible

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Locksmith",
+            dependencies: [],
+            path: "Source"), // Custom path for the Locksmith target source files
+        .testTarget(
+            name: "LocksmithTests",
+            dependencies: ["Locksmith"],
+            path: "Tests/LocksmithTests"), // Custom path for the LocksmithTests target source files
+    ]
 )
